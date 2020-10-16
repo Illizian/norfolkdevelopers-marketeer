b5accc531fe03209529e126e3ae60c1c4587add7
Alex Scotton <alex@alexscotton.com>
2020-10-16 13:36:49 +0100

    Adds the initial Laravel Nova scaffold

---
+++ README.md
@@ -0,0 +1,2 @@
+## nor(DEV): Marketeer
+
---
+++ nova-components/NovaTweetField/src/NovaTwitterField.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Illizian\NovaTweetField;
+
+use Laravel\Nova\Fields\Field;
+
+class NovaTweetField extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'nova-tweet-field';
+}
