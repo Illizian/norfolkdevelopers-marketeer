f32f637782a0b4764408108ea55f370e30185435
Alex Scotton <alex@alexscotton.com>
2020-11-28 21:10:23 +0000

    laravel/jetstream & nova integration > Configure Nova to use Jetstream provided auth

--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -14,9 +14,18 @@
 <dropdown-menu slot="menu" width="200" direction="rtl">
     <ul class="list-reset">
         <li>
-            <a href="{{ route('nova.logout') }}" class="block no-underline text-90 hover:bg-30 p-3">
-                {{ __('Logout') }}
+            <a href="{{ route('profile.show') }}" class="block no-underline text-90 hover:bg-30 p-3">
+                {{ __('Profile') }}
             </a>
+        </li>
+        <li>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <button type="submit" class="block no-underline text-90 hover:bg-30 p-3">
+                    {{ __('Logout') }}
+                </button>
+            </form>
         </li>
     </ul>
 </dropdown-menu>
