5d3640d8b0c3c8d3be3bbeed5517d0ba456d3759
Alex Scotton <alex@alexscotton.com>
2020-11-29 15:42:12 +0000

    Installs spatie/laravel-permissions & vyuldashev/nova-permission for managing permissions

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\Cards\Help;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
+use \Vyuldashev\NovaPermission\NovaPermissionTool;
 
 class NovaServiceProvider extends NovaApplicationServiceProvider
 {
@@ -77,6 +78,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         return [
             new NovaResourceCalendar,
+            new NovaPermissionTool,
         ];
     }
 
